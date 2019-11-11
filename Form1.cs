afd1fd096357b1ed394cd3c0902456a2955869d0
unknown <mysnik_999@mail.ru>
2019-11-11 17:59:29 +0300

    third

--- Form1.cs
+++ Form1.cs
@@ -8,14 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Device.Location;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace WeatherApp
 {
     public partial class Form1 : Form
     {
-
-        public string location;
-        public string currentLocation;
+        public string lon;
+        public string lat;
+        public string currentLon;
+        public string currentLat;
         public string city;
 
         public Form1()
@@ -36,14 +40,31 @@ namespace WeatherApp
 
         private void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
         {
-            location = "dsada"; //String.Format("loc: {0}", e.Position.Location);
-            currentLocation = String.Format("loc: {0}", e.Position.Location);
-            label1.Text = location;
+            lat = String.Format("loc: {0}", e.Position.Location.Latitude);
+            lon = String.Format("loc: {0}", e.Position.Location.Longitude);
+            currentLat = String.Format("loc: {0}", e.Position.Location.Latitude);
+            currentLon = String.Format("loc: {0}", e.Position.Location.Longitude);
+            label1.Text = lat + lon;
             button1.Text = "Текущая локация";
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private async void Form1_Load(object sender, EventArgs e)
         {
+            WebRequest request = WebRequest.Create("https://http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "APPID=1499f87b39982a746c16f0c3ff09b18b&lang=ru");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-urlencoded";
+            WebResponse response = await request.GetResponseAsync();
+            string answer = string.Empty;
+            using (Stream s = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(s))
+                {
+                    answer = await reader.ReadToEndAsync();
+                }
+            }
+            response.Close();
+
+
             GeoCoordinateWatcher watcher = new GeoCoordinateWatcher();
             watcher.PositionChanged += watcher_PositionChanged;
             watcher.Start();
@@ -61,8 +82,9 @@ namespace WeatherApp
 
         private void button1_Click(object sender, EventArgs e)
         {
-            location = currentLocation;
-            label1.Text = location;
+            lon = currentLon;
+            lat = currentLat;
+            label1.Text = lon + lat;
             
         }
 
---
+++ OpenWeather/OpenWeather.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WeatherApp.OpenWeather
+{
+    class OpenWeather
+    {
+        [JsonProperty("base")]
+        public coord coord;
+        public weather[] weather;
+        public string Base;
+        public main main;
+        public double visibility;
+        public wind wind;
+        public clouds clouds;
+        public double dt;
+        public sys sys;
+        public int id;
+        public string name;
+        public double code;
+    }
+}
